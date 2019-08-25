08ce7c841a2b1d7f8721f1a2b7bb35d23816043c
SorinCrainic <sorin.crainic.86@gmail.com>
2019-08-25 09:50:09 +0300

    2019.08.24-Edit paroducts page

--- js/shop.js
+++ js/shop.js
@@ -2,23 +2,6 @@ const API_URL = "http://localhost:8090";
 
 window.Shop = {
 
-    // addProductToCart: function (productId) {
-    //     let body = {
-    //         customerId: 7,
-    //         productId: productId
-    //     };
-    //
-    //     $.ajax({
-    //         url: API_URL + "/cart",
-    //         method: "PUT",
-    //         // MIME type
-    //         contentType: "application/json",
-    //         data: JSON.stringify(body)
-    //     }).done(function () {
-    //         window.location.replace("cart.html");
-    //     });
-    // },
-
     addProductToShoppingCart: function(productToBeAddedToChartId){
             //defining the customer (hardcoded) and product
             let body ={
@@ -50,16 +33,20 @@ window.Shop = {
     getProductHtml: function (product) {
         return `<div class="col-md-3 col-sm-6">
                     <div class="single-shop-product">
-                        <div class="product-upper">
+                        <div class="product-upper" style="height: 250px; width: 300px;">
                             <img src=${product.imagePath} alt="">
                         </div>
-                        <h2><a href="">${product.name}</a></h2>
+
+                        <h2 style="width: 300px; height: 15px;"><a href="">${product.name}</a></h2>
+
+                        <h2 style="width: 300px; height: 15px;"><a href="">Rating: ${product.productRate}</a></h2>
+                        <p href=""style="height: 75px; width: 250px;">Descripion: ${product.productDescription}</p>
                         <div class="product-carousel-price">
-                            <ins>$${product.price}</ins>
-                        </div>  
+                            <ins>Price: € ${product.price}</ins>
+                        </div> 
                         
                         <div class="product-option-shop">
-                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add watch to cart</a>
                         </div>                       
                     </div>
                 </div>`
@@ -73,16 +60,6 @@ window.Shop = {
         // cssSelector
         $('#products-container').html(productsHtml);
     },
-
-    // bindEvents: function () {
-    //     $('#products-container').delegate(
-    //         '.add_to_cart_button', 'click', function (event) {
-    //             event.preventDefault();
-    //
-    //             let productId = $(this).data('product_id');
-    //             Shop.addProductToCart(productId);
-    //     });
-    // }
 
     bindEvents: function () {
         $('#products-container').delegate(
